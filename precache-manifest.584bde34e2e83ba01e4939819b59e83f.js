c3363782edb1fbecca932a7a7908a592e461005c
Pat Perry <risingphx1@gmail.com>
2020-01-04 12:24:30 -0500

    Updates

---
+++ precache-manifest.584bde34e2e83ba01e4939819b59e83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c88a7e2c1d5ced1dfd7d32165c93926",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "9ce69634b1daf8f80af6",
+    "url": "/clickygame/static/css/main.b64fbb33.chunk.css"
+  },
+  {
+    "revision": "73026816a37617a2987b",
+    "url": "/clickygame/static/js/2.b094a780.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.b094a780.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ce69634b1daf8f80af6",
+    "url": "/clickygame/static/js/main.9296717b.chunk.js"
+  },
+  {
+    "revision": "bd9762502ac0aa722f8f",
+    "url": "/clickygame/static/js/runtime-main.e59cb569.js"
+  }
+]);

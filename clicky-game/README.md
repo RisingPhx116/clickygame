56e96d1aa1a81d5e580c829f69b2e515982e2696
Pat Perry <risingphx1@gmail.com>
2020-01-04 12:27:27 -0500

    updated file structure for game

--- clicky-game/README.md
+++
@@ -1,9 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-This is a click game to see if you can click all of the posters ONLY once
-
-Used React and Bootstrap for this.
-
-UNFORTUNATELY my posters are only showing veritcally and HEROKU will not build
-
-App works though :)
---
+++ clicky-game/build/precache-manifest.19665a260d9b827071e3ad3e401ca83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2245ecb578992e15fac28e619fdedb",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "ba48f73296ce2e882e38",
+    "url": "/clickygame/static/css/main.b64fbb33.chunk.css"
+  },
+  {
+    "revision": "5c3f4d58afe38700c6cb",
+    "url": "/clickygame/static/js/2.7463f86d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.7463f86d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba48f73296ce2e882e38",
+    "url": "/clickygame/static/js/main.76a58a14.chunk.js"
+  },
+  {
+    "revision": "b9f059775642ea22eec0",
+    "url": "/clickygame/static/js/runtime-main.fe043e56.js"
+  }
+]);
